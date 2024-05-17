644fdad366ed4fe2c5e07ccf657fd141b73f2a7d
Cynthia Valles <cynthiavalles8299@gmail.com>
2024-05-16 23:00:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send('Server Error!');
+})
